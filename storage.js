0e5b775dc0a27049fc6f339e31a977f2af7989fb
Vitor Santos <vsantos.nasc@gmail.com>
2015-03-29 19:38:57 -0300

    Add all functions inside of object. For have private functions

--- storage.js
+++ storage.js
@@ -1,206 +1,204 @@
 
 var DB = function(name) {
-	this.name = name;
 
-	this.checkOrCreate();
-
-};
-
-DB.prototype.checkOrCreate = function() {
-	if (localStorage.getItem(this.name) == null) {
-		localStorage.setItem(this.name, "[]");
-	}
-};
-
-DB.prototype.getData = function() {
-	var data = localStorage.getItem(this.name);
-	data = JSON.parse(data);
-	return data;
-};
-
-DB.prototype.setData = function(data) {
-	localStorage.setItem(this.name, JSON.stringify(data));
-};
-
-DB.prototype.all = function() {
-	return this.getData();
-};
-
-DB.prototype.insert = function(new_data,uuid) {
-	if (typeof new_data != 'object') {
-		throw "Required object";
-	}
-
-	if( typeof uuid == 'undefined')
-		new_data._uuid = this.createUuid();
-	else
-		new_data._uuid = uuid;
-
-	new_data._created_at = new Date(Date.now());
-	new_data._modified_at = new Date(Date.now());
-
-	this.checkOrCreate();
-
-	var data = this.getData();
-	data.push(new_data);
-
-	this.setData(data);
-
-	return new_data;
-
-};
-
-DB.prototype.insertAll = function(list){
-	if(Array.isArray(list))
-		throw "Required array";
-
-	this.checkOrCreate();
-	
-	var data = this.getData();
-	for (var i = 0; i < list.length; i++){
-		list[i]._uuid = this.createUuid();
-		list[i]._created_at = new Date(Date.now());
-		list[i]._modified_at = new Date(Date.now());
-		data.push(new_data);
-	}
-	return data;
-}
-
-DB.prototype.delete = function(where) {
-	if (this.checkUuid(where)) {
-		where = {
-			_uuid: where
-		};
-	}
-
-	var data = this.getData();
-	var new_data = [];
-	for (var i = 0; i < data.length; i++) {
-
-		if (!this.isEquals(data[i], where)) {
-			new_data.push(data[i]);
+	var checkOrCreate = function() {
+		if (localStorage.getItem(this.name) == null) {
+			localStorage.setItem(this.name, "[]");
 		}
-
-	};
-	this.setData(new_data);
-	return;
-};
-
-DB.prototype.select = function(where) {
-	if (this.checkUuid(where)) {
-		where = {
-			_uuid: where
-		};
-	}
-
-	var data = this.getData();
-	var select_data = [];
-	for (var i = 0; i < data.length; i++) {
-
-		if (this.isEquals(data[i], where)) {
-			select_data.push(data[i]);
-		}
-
 	};
 
-	return select_data;
-};
+	var checkUuid = function(uuid) {
+		if (typeof uuid != 'string')
+			return false;
 
-DB.prototype.update = function(data_updated, where) {
-	if (this.checkUuid(where)) {
-		where = {
-			_uuid: where
-		};
-	}
-
-	var data = this.getData();
-	var new_data = [];
-	var changed = [];
-	for (var i = 0; i < data.length; i++) {
-
-		if (this.isEquals(data[i], where)) {
-			var self_data = this.updateValue(data[i], data_updated);
-			new_data.push(self_data);
-			changed.push(self_data)
-		} else {
-			new_data.push(data[i]);
+		if (uuid.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)) {
+			return true;
 		}
 
-	};
-
-	this.setData(new_data);
-
-	return changed;
-
-};
-
-DB.prototype.drop = function() {
-	localStorage.removeItem(this.name);
-};
-
-DB.prototype.clear = function(){
-	this.drop();
-	this.checkOrCreate();
-}
-
-
-DB.prototype.isEquals = function(row, where) {
-
-	var t = 0;
-	for (var key in row) {
-		if (row.hasOwnProperty(key) && row[key] != null && row[key] == where[key]) {
-			t++;
-		}
-	}
-
-	if (t == Object.keys(where).length) {
-		return true;
-	} else {
 		return false;
-	}
+	};
 
-};
+	var updateValue = function(obj) {
+		for (var i = 1; i < arguments.length; i++) {
+			for (var prop in arguments[i]) {
+				var val = arguments[i][prop];
+				if (typeof val == "object" && !Array.isArray(val)) { // this also applies to arrays or null!
+					updateValue(obj[prop], val);
+				} else {
+					try {
 
-DB.prototype.createUuid = function() {
-	var d = new Date().getTime();
-	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-		var r = (d + Math.random() * 16) % 16 | 0;
-		d = Math.floor(d / 16);
-		return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-	});
-	return uuid;
-};
+						obj[prop] = val;
 
-DB.prototype.checkUuid = function(uuid) {
-	if (typeof uuid != 'string')
-		return false;
+					} catch (e) {
 
-	if (uuid.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)) {
-		return true;
-	}
-
-	return false;
-};
-
-DB.prototype.updateValue = function(obj) {
-	for (var i = 1; i < arguments.length; i++) {
-		for (var prop in arguments[i]) {
-			var val = arguments[i][prop];
-			if (typeof val == "object" && !Array.isArray(val)) { // this also applies to arrays or null!
-				this.updateValue(obj[prop], val);
-			} else {
-				try {
-
-					obj[prop] = val;
-
-				} catch (e) {
-
-					console.log(e);
+						console.log(e);
+					}
 				}
 			}
 		}
+
+		obj._modified_at = new Date(Date.now());
+		return obj;
+	};
+
+	var getData = function() {
+		var data = localStorage.getItem(this.name);
+		data = JSON.parse(data);
+		return data;
+	};
+	var setData = function(data) {
+		localStorage.setItem(this.name, JSON.stringify(data));
+	};
+
+	var createUuid = function() {
+		var d = new Date().getTime();
+		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = (d + Math.random() * 16) % 16 | 0;
+			d = Math.floor(d / 16);
+			return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+		});
+		return uuid;
+	};
+
+	var isEquals = function(row, where) {
+
+		var t = 0;
+		for (var key in row) {
+			if (row.hasOwnProperty(key) && row[key] != null && row[key] == where[key]) {
+				t++;
+			}
+		}
+
+		if (t == Object.keys(where).length) {
+			return true;
+		} else {
+			return false;
+		}
+
+	};
+
+	this.all = function() {
+		return getData();
+	};
+
+	this.insert = function(new_data) {
+
+		if (typeof new_data != 'object') {
+			throw "Required object";
+		}
+
+		new_data._uuid = createUuid();
+
+
+		new_data._created_at = new Date(Date.now());
+		new_data._modified_at = new Date(Date.now());
+
+		checkOrCreate();
+
+		var data = getData();
+		data.push(new_data);
+
+		setData(data);
+
+		return new_data;
+
+	};
+
+	this.insertAll = function(list){
+		if(Array.isArray(list))
+			throw "Required array";
+
+		checkOrCreate();
+		
+		var data = getData();
+		for (var i = 0; i < list.length; i++){
+			list[i]._uuid = createUuid();
+			list[i]._created_at = new Date(Date.now());
+			list[i]._modified_at = new Date(Date.now());
+			data.push(new_data);
+		}
+		return data;
 	}
 
-	obj._modified_at = new Date(Date.now());
-	return obj;
+	this.delete = function(where) {
+		if (checkUuid(where)) {
+			where = {
+				_uuid: where
+			};
+		}
+
+		var data = getData();
+		var new_data = [];
+		for (var i = 0; i < data.length; i++) {
+
+			if (!isEquals(data[i], where)) {
+				new_data.push(data[i]);
+			}
+
+		};
+		setData(new_data);
+		return;
+	};
+
+	this.select = function(where) {
+		if (checkUuid(where)) {
+			where = {
+				_uuid: where
+			};
+		}
+
+		var data = getData();
+		var select_data = [];
+		for (var i = 0; i < data.length; i++) {
+
+			if (isEquals(data[i], where)) {
+				select_data.push(data[i]);
+			}
+
+		};
+
+		return select_data;
+	};
+	this.update = function(data_updated, where) {
+
+		if (checkUuid(where)) {
+
+			where = {
+				_uuid: where
+			};
+		}
+
+		var data = getData();
+		var new_data = [];
+		var changed = [];
+		for (var i = 0; i < data.length; i++) {
+
+			if (isEquals(data[i], where)) {
+				var self_data = updateValue(data[i], data_updated);
+				new_data.push(self_data);
+				changed.push(self_data)
+			} else {
+				new_data.push(data[i]);
+			}
+
+		};
+
+		setData(new_data);
+
+		return changed;
+
+	};
+	this.drop = function() {
+		localStorage.removeItem(this.name);
+	};
+
+	function __construct(){
+
+		this.name = name;
+
+		checkOrCreate();
+
+	}
+	__construct();
+
 };
